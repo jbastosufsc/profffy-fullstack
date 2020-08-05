94ca65a1950d76ff9258ce934718d554b9c30c02
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-05 12:09:08 -0300

    Add style to landing page desktop version

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -28,7 +28,7 @@ function Landing() {
 
           <a href='' className='give-classes'>
             <img src={giveClassesIcon} alt='' />
-            Estudar
+            Dar aulas
           </a>
         </div>
         <span className='total-connections'>
