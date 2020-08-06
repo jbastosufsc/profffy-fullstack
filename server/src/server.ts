2b8196b97ff90809a2c88ef40243d1a3919a972d
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-06 16:05:25 -0300

    Install and run express

---
+++ server/src/server.ts
@@ -0,0 +1,9 @@
+import express, { response } from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'hello world' });
+});
+app.listen(3333);
