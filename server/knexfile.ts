f56546e903c78d6e54fbbf92f620f977bc210b8e
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-11 09:58:21 -0300

    Create database connection files

---
+++ server/knexfile.ts
@@ -0,0 +1,14 @@
+// Needed file because knex doesn't understand typescript
+
+import path from 'path';
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+  useNullAsDefault: true,
+};
