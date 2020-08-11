f63eb5a8d98a095290f6513b010526cbde1b3ab4
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-11 16:21:06 -0300

    Fix ref table for user_id in class_schedule

--- server/src/database/migrations/02_create_class_schedule.ts
+++ server/src/database/migrations/02_create_class_schedule.ts
@@ -12,7 +12,7 @@ export async function up(knex: Knex) {
       .integer('user_id')
       .notNullable()
       .references('id')
-      .inTable('users')
+      .inTable('classes')
       .onUpdate('CASCADE')
       .onDelete('CASCADE');
   });
