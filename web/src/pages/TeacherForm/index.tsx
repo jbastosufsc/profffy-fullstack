3a4537a6b1d6047b42f07bd9e66fdb7f46af0673
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-20 10:28:13 -0300

    Create and style Select component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-import './styles.css';
+import TextArea from '../../components/TextArea';
+import Select from '../../components/Select';
 import Input from '../../components/Input';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
+import './styles.css';
 
 function TeacherForm() {
   return (
@@ -18,10 +20,24 @@ function TeacherForm() {
           <Input label='Nome completo' name='name' />
           <Input label='Avatar' name='avatar' />
           <Input label='Whatsapp' name='whatsapp' />
+          <TextArea label='Biografia' name='bio' />
         </fieldset>
         <fieldset>
           <legend>Sobre a aula</legend>
-          <Input label='Materia' name='subject' />
+          <Select
+            label='Materia'
+            name='subject'
+            options={[
+              { value: 'artes', name: 'Artes' },
+              { value: 'portugues', name: 'Português' },
+              { value: 'matematica', name: 'Matemática' },
+              { value: 'geografia', name: 'Geografia' },
+              { value: 'fisica', name: 'Física' },
+              { value: 'historia', name: 'História' },
+              { value: 'quimica', name: 'Química' },
+              { value: 'biologia', name: 'Biologia' },
+            ]}
+          />
           <Input label='Custo por hora da aula' name='cost' />
         </fieldset>
         <footer>
