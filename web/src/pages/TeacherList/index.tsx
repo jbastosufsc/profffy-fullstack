92f28a9d5f94765b2a5f3d780b61c60a0e619f39
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-20 11:20:33 -0300

    Add Select component to TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import './styles.css';
 
 function TeacherList() {
   return (
     <div id='page-teacher-list' className='container'>
       <PageHeader title='Estes são os proffys disponíveis'>
         <form id='search-teachers'>
-          <Input label='Matéria' name='subject' />
-          <Input label='Dia da semana' name='week-day' />
+          <Select
+            label='Materia'
+            name='subject'
+            options={[
+              { value: 'artes', name: 'Artes' },
+              { value: 'portugues', name: 'Português' },
+              { value: 'matematica', name: 'Matemática' },
+              { value: 'geografia', name: 'Geografia' },
+              { value: 'fisica', name: 'Física' },
+              { value: 'historia', name: 'História' },
+              { value: 'quimica', name: 'Química' },
+              { value: 'biologia', name: 'Biologia' },
+            ]}
+          />
+          <Select
+            label='Dia da semana'
+            name='week-day'
+            options={[
+              { value: '0', name: 'Domingo' },
+              { value: '1', name: 'Segunda-feira' },
+              { value: '2', name: 'Terça-feira' },
+              { value: '3', name: 'Quarta-feira' },
+              { value: '4', name: 'Quinta-feira' },
+              { value: '5', name: 'Sexta-feira' },
+              { value: '6', name: 'Sábado' },
+            ]}
+          />
           <Input type='time' label='Hora' name='time' />
         </form>
       </PageHeader>
