edd855f3e9c7639f3976fe18501a707e9747d60c
jbastosufsc <jbastos.ufsc@gmail.com>
2020-08-19 21:40:44 -0300

    Add styles to form and create Input component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import './styles.css';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 function TeacherForm() {
   return (
-    <div id='page-teacher-list' className='container'>
+    <div id='page-teacher-form' className='container'>
       <PageHeader
         title='Que incrível que você quer dar aulas.'
         description='O primeiro passo é preencher o formulário de inscrição'
       />
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input label='Nome completo' name='name' />
+          <Input label='Avatar' name='avatar' />
+          <Input label='Whatsapp' name='whatsapp' />
+        </fieldset>
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input label='Materia' name='subject' />
+          <Input label='Custo por hora da aula' name='cost' />
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt='Aviso importante' />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type='button'>Salvar cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
